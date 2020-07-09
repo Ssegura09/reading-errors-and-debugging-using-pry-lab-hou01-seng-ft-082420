3c14ac80e9baf4464f7b5a21328bee38852b980f
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-07-09 01:19:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -10,8 +10,8 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage(state_log)
-  return "#{state_log} and crew greetings."
+def engage(star_date)
+  return "Captain's Log, star date #{star_date}.
 end
 
 
