a48a4c19d73d32dc948d91e9a86454edc2d3e120
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-07-09 01:29:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -9,15 +9,8 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def crew
-  crew = ""
-end
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
-end
-
-def engage
+def engage(greet_crew)
   date = generate_star_date
   state_log(date)
   greet_crew(crew)
