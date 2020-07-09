58ba7070510ae3d0d0c1f6a749ccae10c8366384
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-07-09 01:13:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,7 +1,8 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  binding.pry
 end
 
 
@@ -9,7 +10,9 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage
-  puts state_log(date)
+def engage(greeting)
+  returns state_log #{greeting}
   date = generate_star_date
 end
+
+
